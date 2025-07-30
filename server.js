bcbee94a5932e98865333eb401efa71db3958c16
Grant Roleplay <your-email@example.com>
2025-07-30 22:16:12 +0300

    Update database host to RAGEMP server IP

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Aqua Database bağlantısı
 const dbConfig = {
-    host: '127.0.0.1',
+    host: '192.168.1.20',
     user: 'root',
     password: '',
     database: 'aqua',
